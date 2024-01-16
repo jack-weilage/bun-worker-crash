c7cdd5f4fe18774d56fcff8693c0ffc55e288383
Jack Weilage <jack@weilage.dev>
2024-01-16 12:04:24 -0800

    Decrease iterations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Worker } from "worker_threads";
 
 async function run() {
-	for (let i = 0; i < 1_000_000; i++) {
+	for (let i = 0; i < 1_000; i++) {
 		await new Promise((resolve) => {
 			const worker = new Worker("./worker.js");
 
