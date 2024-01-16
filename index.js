d9fa0b1bc7561635d357373f7f531365e54db7d6
Jack Weilage <jack@weilage.dev>
2024-01-16 11:55:08 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import { Worker } from "worker_threads";
+
+async function run() {
+	for (let i = 0; i < 1_000_000; i++) {
+		await new Promise((resolve) => {
+			const worker = new Worker("./worker.js");
+
+			worker.on("message", () => resolve(1));
+		});
+
+		console.log(i);
+	}
+}
+
+run();
